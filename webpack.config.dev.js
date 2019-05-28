0385f6ce0a54065df69b5a98f1302f429a8d9372
Mustafa Khamisi <mustafangure95@gmail.com>
2019-05-28 18:06:14 +0300

    feat: Configure webpack to run server and bundle code

---
+++ webpack.config.dev.js
@@ -0,0 +1,45 @@
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+process.env.NODE_ENV = "development";
+
+module.exports = {
+  mode: "development",
+  target: "web",
+  devtool: "cheap-module-source-map",
+  entry: "./src/index", // this is the default. it doesn't need to be added.
+  output: {
+    // webpack doesnt output code in development mode. It serves our app from memory.
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    stats: "minimal", // reduces info written to the command line
+    overlay: true, // overlay errors in the browser
+    historyApiFallback: true, // all requests will be sent to index.html
+    disableHostCheck: true,
+    headers: { "Access-Control-Allow-Origin": "*" },
+    https: false
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      favicon: "./src/favicon.ico"
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_nodules/,
+        use: ["babel-loader", "eslint-loader"]
+      },
+      {
+        test: /(\.css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
