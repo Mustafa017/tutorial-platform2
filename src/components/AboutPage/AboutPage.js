023cc048df88645763554f552cba77beb32f50d1
Mustafa Khamisi <mustafangure95@gmail.com>
2019-06-04 16:28:02 +0300

    feat: Create about page

---
+++ src/components/AboutPage/AboutPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      This site offers a course management system to keep track of your learning
+      map.
+    </p>
+  </div>
+);
+
+export default AboutPage;
