fcb469ea3d38c2eba8c189bcc98c20cb4aa087cd
Mustafa Khamisi <mustafangure95@gmail.com>
2019-06-25 20:38:36 +0300

    feat: Add first thunk called loadCourse

--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,5 +1,24 @@
 import * as types from "../actions/actionTypes";
+import * as courseApi from "../../api/courseApi";
 
 export function createCourse(course) {
   return { type: types.CREATE_COURSE, course: course };
+}
+
+export function loadCourseSuccess(courses) {
+  // use object destructuring {courses : courses}
+  return { type: types.LOAD_COURSE_SUCCESS, courses };
+}
+
+export function loadCourse() {
+  return function(dispatch) {
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
 }
